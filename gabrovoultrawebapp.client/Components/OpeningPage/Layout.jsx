67dce835ab0cef6562a4cab18efaadbb6ae51830
Михаел Йорданов <gaws@mail.bg>
2024-04-10 10:27:57 +0300

    Refactor Layout so it showls everything

--- gabrovoultrawebapp.client/Components/OpeningPage/Layout.jsx
+++ gabrovoultrawebapp.client/Components/OpeningPage/Layout.jsx
@@ -1,42 +1,17 @@
 import "./styles.css";
 import AboutUs from "../AboutUs";
-import CustomNav from "../CustomNav/CustomNav";
-import { Component, useEffect, useState } from "react";
-import Title from "../Title/Title";
-import DistancesHeader from "../Distances/Header/DistancesHeader";
-import DustanceMain from "../Distances/Main/DistanceMain";
-import { Container } from "react-bootstrap";
+import Distances from "../Distances/Distances/Distances";
+import Header from "../Header/Header";
+import { Gallery } from "../Gallery/Gallery";
+import AllRunners from "../AllRunners";
 
 export default function Layout() {
-  const [display, setDisplay] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      if (window.scrollY > 400) {
-        setDisplay(true);
-      } else {
-        setDisplay(false);
-      }
-      if (localStorage.getItem("token")) {
-        setLoggedIn(true);
-      }
-    });
-  }, []);
-
   return (
-    <div id="Home">
-      <CustomNav
-        loggedIn={loggedIn}
-        display={display}
-        dynamicStyles={display ? `visible` : ""}
-      />
-      <Container fluid className="banner container">
-        <Title />
-      </Container>
+    <div id="Home" className="">
+      <Header />
       <AboutUs />
-      <DistancesHeader id="Distances" />
-      <DustanceMain />
+      <Distances />
+      <Gallery />
     </div>
   );
 }
